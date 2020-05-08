832892c509310768d62c2ff774dbf552833685f9
Coolbrother <coolbrother@sourcegod.com>
2020-05-08 22:11:43 +0200

    v0.22 - logger 2

--- src/logger.hpp
+++ src/logger.hpp
@@ -2,8 +2,81 @@
 #define LOGGER_HPP
 #include <iostream>
 #include <string>
-
+#include <chrono> // for datetime
+#include <ctime> // to convert chrono datetime
+#include <sstream>
 // #define DEBUG  1 
+// Simple object logging
+enum TLogLevel { 
+  log_OFF, log_DEBUG, log_INFO, log_WARN, log_ERROR 
+};
+
+struct TLog {
+  bool headers = false;
+  TLogLevel level = log_OFF;
+};
+
+extern TLog LogConf;
+
+class CLog {
+public:
+  CLog() { CLog(log_DEBUG); }
+  CLog(TLogLevel level) {
+    m_level = level;
+    if (m_level > log_OFF && m_level <= LogConf.level && LogConf.headers) {
+      dateNow();
+      operator << (" [" + getLabel(level) + "] ");
+    }
+  }
+  
+  ~CLog() {
+    if (m_opened) {
+      std::cerr << "\n";
+    }
+    m_opened = false;
+  }
+  
+  template <class T>
+  CLog &operator <<(const T &msg) {
+    if (m_level > log_OFF && m_level <= LogConf.level) {
+        std::cerr << msg;
+        m_opened = true;
+    }
+
+    return *this;
+  }
+
+  // std::string getDate() {
+  void dateNow() {
+    auto start = std::chrono::system_clock::now();
+    std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+    std::cerr << "Date: " << std::ctime(&start_time);
+    // oss << std::ctime(&start_time);
+
+    // return oss.str();
+
+  }
+
+private:
+  bool m_opened = false;
+  TLogLevel m_level = log_DEBUG;
+  std::ostringstream oss;
+  
+  inline std::string getLabel(TLogLevel lvl) {
+    std::string label;
+    switch(lvl) {
+      case log_OFF: label = "OFF "; break;
+      case log_DEBUG: label = "DEBUG "; break;
+      case log_INFO: label = "INFO "; break;
+      case log_WARN: label = "WARN "; break;
+      case log_ERROR: label = "ERROR "; break;
+    }
+
+    return label;
+  }
+
+};
+
 // object to tracing
 class CTracer {
 public:
@@ -38,46 +111,34 @@ private:
 // variadic template
 // base case
 template <typename T>
-void logMsg(T val) { 
-#ifdef DEBUG
-        std::cerr << val << "\n";
-            // << __PRETTY_FUNCTION__ << ", " << __FILE__ << ":" << __LINE__ << "\n";
-#endif
+void logMsg(T val) {
+  if (LogConf.level >= log_DEBUG) {
+    std::cerr << val << "\n";
+  }
 
 }
 
 // recursive case
 template <typename T, typename... TArgs>
 void logMsg(T first, TArgs... args) {
-#ifdef DEBUG
-        std::cerr << first << " ";
-        logMsg(args...);
-#endif
+  if (LogConf.level >= log_DEBUG) {
+    std::cerr << first << " ";
+    logMsg(args...);
+  }
 
 }
 
 
 // macros to logging
 #ifdef DEBUG
-    // pass argument to CTracer object
-    #define TRACE_ENTRY_EXIT_FOR(x)       CTracer varLogObj(x)
-    // print only unction name
-    #define TRACE_ENTRY_EXIT              TRACE_ENTRY_EXIT_FOR(__func__)
-    // print all arguments as file, line, and function name
-    #define TRACE_ENTRY_EXIT_ALL(x, y, z)       CTracer varLogAll(x, y, z)
-    // print by default file, line, and function name without arguments
-    #define TRACE_ALL              TRACE_ENTRY_EXIT_ALL(__FILE__, __LINE__, __PRETTY_FUNCTION__)
-    
+   
     // print by default file, line, and function name
     #define LOG_FILE              std::cerr << __FILE__ \
         << ":" << __LINE__ << " " << __PRETTY_FUNCTION__ << "\n";
     // print string value for the variable name and the content
     #define LOG_DUMP(x)         do { std::cerr << #x " is value " << (x) << ", "; LOG_FILE; } while(false)
     
-    // print message and all traces like file, line, and function name
-    #define TRACE_MSG(msg)         std::cerr << msg << "\n"; \
-                                    TRACE_ALL
-    
+       
     // print an message with file, line, and functions name
     #define DEBUG_MSG(msg)       std::cerr << msg << "\n" \
                                     << __FILE__ << ":" << __LINE__ << " " << __PRETTY_FUNCTION__ << "\n";
@@ -86,15 +147,31 @@ void logMsg(T first, TArgs... args) {
         do { if (DEBUG) { fprintf(stderr, fmt, __VA_ARGS__); \
             fprintf(stderr, "\n");} } while (0)
 #else
-    #define TRACE_ENTRY_EXIT
-    #define TRACE_ALL
     #define LOG_FILE
     #define LOG_DUMP
-    #define TRACE_MSG
     #define DEBUG_MSG
     #define DEBUG_PRINT
 
-#endif
+#endif // DEBUG
 
+#ifdef TRACER
+    // pass argument to CTracer object
+    #define TRACE_ENTRY_EXIT_FOR(x)       CTracer varLogObj(x)
+    // print only unction name
+    #define TRACE_ENTRY_EXIT              TRACE_ENTRY_EXIT_FOR(__func__)
+    // print all arguments as file, line, and function name
+    #define TRACE_ENTRY_EXIT_ALL(x, y, z)       CTracer varLogAll(x, y, z)
+    // print by default file, line, and function name without arguments
+    #define TRACE_ALL              TRACE_ENTRY_EXIT_ALL(__FILE__, __LINE__, __PRETTY_FUNCTION__)
+    // print message and all traces like file, line, and function name
+    #define TRACE_MSG(msg)         std::cerr << msg << "\n"; \
+                                    TRACE_ALL
+
+#else
+    #define TRACE_ENTRY_EXIT
+    #define TRACE_ALL
+    #define TRACE_MSG
+
+#endif // TRACER
 
 #endif // LOGGER_HPP
