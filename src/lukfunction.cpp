706d11bb83d5547335eb4adc834bbce397e3e7e4
Coolbrother <coolbrother@sourcegod.com>
2020-08-09 00:50:47 +0200

    Changing LukObject to LukObject shared pointer in LukFunction class part 3.

--- src/lukfunction.cpp
+++ src/lukfunction.cpp
@@ -26,7 +26,7 @@ ObjPtr  LukFunction::call(Interpreter& interp, std::vector<ObjPtr>& v_args) {
     if (m_isInitializer) return  m_closure->getAt(0, "this");
     
     
-    return TObject();
+    return nilptr;
 }
 
 ObjPtr LukFunction::bind(std::shared_ptr<LukInstance> instPtr) {
