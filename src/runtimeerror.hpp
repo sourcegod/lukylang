fa67fd2f79080d515e6ebcfbd576904acb782b95
Coolbrother <coolbrother@sourcegod.com>
2020-08-26 04:50:45 +0200

    Added file: common.hpp, in RuntimeError class.

--- src/runtimeerror.hpp
+++ src/runtimeerror.hpp
@@ -1,6 +1,7 @@
 #ifndef RUNTIMEERROR_HPP
 #define RUNTIMEERROR_HPP
 
+#include "common.hpp"
 #include "token.hpp"
 #include <stdexcept> // runtime_error
 #include <string>
@@ -9,10 +10,12 @@ class RuntimeError : public std::runtime_error {
     
 public:
     RuntimeError(const std::string& msg) 
-        : std::runtime_error(msg) {}
+        : std::runtime_error(msg) 
+    {}
 
     RuntimeError(TokPtr& tok, const std::string& msg) 
-        : std::runtime_error(tok->lexeme + ", " + msg) {}
+        : std::runtime_error(tok->lexeme + ", " + msg) 
+    {}
 
 };
 
