9695788bd201d1b808d4f5f5106aa7358d1e2679
Coolbrother <coolbrother@sourcegod.com>
2020-08-26 04:45:10 +0200

    Added file: common.hpp, in jump class.

--- src/jump.hpp
+++ src/jump.hpp
@@ -1,17 +1,18 @@
 #ifndef JUMP_HPP
 #define JUMP_HPP
 
-#include "token.hpp"
+#include "common.hpp"
 #include <stdexcept> // runtime_error
 #include <string>
 
 class Jump : public std::runtime_error {
-    
 public:
     Jump(TokPtr& keyword, const std::string& msg) 
         : std::runtime_error(keyword->lexeme + ", " + msg), 
-        m_keyword(keyword) {} 
-    TokPtr m_keyword;
+        m_keyword(keyword) 
+      {} 
+
+TokPtr m_keyword;
 };
 
 #endif //  JUMP_HPP
