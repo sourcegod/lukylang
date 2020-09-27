de5274350db203b6a3d08201b5094bc904cf9887
Coolbrother <coolbrother@sourcegod.com>
2020-09-27 13:10:25 +0200

    Updated file: utils/run_test.sh to exclude file: native_readln.luk for input reasons.

--- utils/run_test.sh
+++ utils/run_test.sh
@@ -8,7 +8,13 @@ testDir="$scriptDir/../tests"
 progName="$scriptDir/../build/release/luky"
 # echo "voici scriptDir: $scriptDir, et testDir: $testDir"
 for fname in $testDir/*.luk; do
+  name=$(basename $fname)
+  if [ $name = "17_03_native_readln.luk" ]; then
+    continue
+  fi
+  # echo $name
   $progName $fname
 done
-
+echo
+echo "End test";
 exit 0
