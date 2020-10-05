b0c3d38cdeaed9e178a3e9c4c8601892b9214d52
Coolbrother <coolbrother@sourcegod.com>
2020-10-05 23:42:16 +0200

    Updated version to v0.02: scanner

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,68 @@ using namespace std;
 // test macro from the preprocessor to retriev string
 #define stringify( name ) #name
 
+namespace luky {
+
+    static void printer(const vector<Token>& v_tokens) {
+        size_t pos =0;
+        cout << "Scanner Tokens list" << endl;
+        for (auto it: v_tokens) {
+            cout << "Pos: " << pos << ", name: " << it.toString() 
+                << " line: " << it.line << ":" << it.col << endl; 
+            pos++;
+        }
+        cout << "Finish..." << endl;
+
+    }
+
+    static void run(const std::string& source, ErrorHandler& errorHandler) {
+        /// scanner
+        Scanner scanner(source, errorHandler);
+        const auto tokens = scanner.scanTokens();
+        if (errorHandler.hadError) return;
+        // printer
+        printer(tokens);
+        /*
+        /// parser
+        // Parser parser(tokens, errorHandler);
+        // auto expr = parser.parse();
+        // if found error during parsing, report
+        if (errorHandler.foundError) {
+            errorHandler.report();
+            return;
+        }
+        */
+        
+        /*
+        /// print ast
+        ASTPrinter pp;
+        pp.print(expr);
+        std::cout << std::endl;
+        */
+
+    }
+
+    static void runFile(const std::string& path, ErrorHandler& errorHandler) {
+        std::ifstream file(path);
+        std::ostringstream stream;
+        stream << file.rdbuf();
+        file.close();
+        run(stream.str(), errorHandler);
+    }
+
+    static void runPrompt(ErrorHandler& errorHandler) {
+        std::string line;
+        while (1) {
+            std::cout << "-> ";
+            // manage ctrl-D to exit the input loop
+            if (!getline(std::cin, line)) break;
+            run(line, errorHandler);
+            errorHandler.hadError = false;
+        }
+
+    }
+}
+
 static void test() {
     luky::ErrorHandler err;
     const std::string source = "1+2; while { print }";
@@ -25,6 +87,16 @@ static void test() {
 }
 
 int main(int argc, char* argv[]) {
-    test();
+    // test();
+    luky::ErrorHandler err;
+    if (argc >2) {
+        cout << "Usage: luky [filename]" << endl;
+    } else if (argc == 2) {
+        cout << "Run file " << argv[1] << endl;
+        luky::runFile(argv[1], err);
+    } else {
+        luky::runPrompt(err);
+    }
+
     return 0;
 }
