56cd7fe8a033d16e92d846d8d749029a5d11012f
Coolbrother <coolbrother@sourcegod.com>
2020-10-06 16:16:17 +0200

    Updated to version 0.06: Arithmetic operators

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,9 +1,12 @@
 #include "interpreter.hpp"
+// #include "lukerror.hpp"
+#include "runtimeerror.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
 
 Interpreter::Interpreter() {}
+    // : lukErr(_lukErr) {}
 
 std::string Interpreter::print(Expr* expr) {
     expr->accept(*this);
@@ -17,12 +20,92 @@ void Interpreter::interpret(PExpr& expression) {
         auto val = evaluate(expression);
         // std::cout << m_result << std::endl;
         std::cout << stringify(val) << std::endl;
-    } catch (std::runtime_error err) {
+    // } catch (std::runtime_error err) {
+    // Note: passing exception by reference to avoid copy
+    } catch (RuntimeError& err) {
         std::cerr << "Interpreter Error: " << err.what() << "\n";
     }
 
     return;
 }
+
+TObject Interpreter::visitBinaryExpr(BinaryExpr& expr) {
+    // method get allow to convert smart pointer to raw pointer
+    TObject left = evaluate(expr.left);
+    TObject right = evaluate(expr.right);
+    switch(expr.op.type) {
+        case TokenType::MINUS:
+            checkNumberOperands(expr.op, left, right);
+            return (double)left - (double)right;
+        
+        case TokenType::PLUS:
+            if (left.isNumber() && right.isNumber())
+                return (double)left + (double)right; 
+        case TokenType::SLASH:
+            checkNumberOperands(expr.op, left, right);
+            return (double)left / (double)right;
+
+        case TokenType::STAR:
+            checkNumberOperands(expr.op, left, right);
+            return (double)left * (double)right;
+         default: break;
+    }
+
+    /*
+    TObject obj =  parenthesize(expr.op.lexeme, 
+            {expr.left.get(), expr.right.get()});
+    */
+
+    return TObject();
+}
+
+TObject Interpreter::visitGroupingExpr(GroupingExpr& expr) {
+    return evaluate(expr.expression);
+    // return TObject();
+}
+
+
+TObject Interpreter::visitLiteralExpr(LiteralExpr& expr) {
+    // m_result += expr.value.toString();
+    return expr.value;
+
+}
+
+TObject Interpreter::visitUnaryExpr(UnaryExpr& expr) {
+    TObject right = evaluate(expr.right);
+    switch(expr.op.type) {
+        case TokenType::BANG:
+            return !isTruthy(right);
+        
+        case TokenType::MINUS:
+            checkNumberOperand(expr.op, right);
+            return -(double)right;
+        default: break;
+    }
+
+
+    return TObject();
+}
+
+bool Interpreter::isTruthy(TObject& obj) {
+    if (obj.isNil()) return false;
+    if (obj.isBool()) return (bool)obj;
+
+    return true;
+}
+void Interpreter::checkNumberOperand(Token& op, TObject& operand) {
+    if (operand.isNumber()) return;
+    // throw std::runtime_error("From throw: Operand must be number.");
+    throw RuntimeError(op, "Operand must be number.");
+}
+
+void Interpreter::checkNumberOperands(Token& op, TObject& left, TObject& right) {
+    if (left.isNumber() && right.isNumber()) return;
+    throw RuntimeError(op, "Operands must be numbers.");
+    // throw std::runtime_error("Yes, Operands must be numbers.");
+
+}
+
 
 std::string Interpreter::parenthesize(std::string name, std::vector<Expr*> v_expr) {
     m_result += "(" + name;
@@ -36,18 +119,20 @@ std::string Interpreter::parenthesize(std::string name, std::vector<Expr*> v_exp
     return m_result;
 }
 
-TObject Interpreter::visitBinaryExpr(BinaryExpr& expr) { return TObject(); }
-TObject Interpreter::visitGroupingExpr(GroupingExpr& expr) { return TObject(); }
-TObject Interpreter::visitLiteralExpr(LiteralExpr& expr) { 
-  return expr.value; 
-}
-TObject Interpreter::visitUnaryExpr(UnaryExpr& expr) { return TObject(); }
-
 TObject Interpreter::evaluate(PExpr& expr) { 
     return expr->accept(*this);
 }
 
 std::string Interpreter::stringify(TObject obj) { 
+    if (obj.isNumber()) {
+        std::string text = obj.toString(); 
+        std::string end = ".000000";
+        // extract decimal part if ending by .0
+        if (endsWith(text, end)) 
+            return text.substr(0, text.size() - end.size());
+        return text;
+    }
+
     return obj.toString();
 }
 
--- src/interpreter.hpp
+++ src/interpreter.hpp
@@ -8,19 +8,34 @@
 class Interpreter : public ExprVisitor {
 public:
     Interpreter();
+
     std::string print(Expr* expr);
     void interpret(PExpr& expression);
     TObject visitBinaryExpr(BinaryExpr& expr);
     TObject visitGroupingExpr(GroupingExpr& expr);
     TObject visitLiteralExpr(LiteralExpr& expr); 
     TObject visitUnaryExpr(UnaryExpr& expr);
+    void checkNumberOperand(Token& op, TObject& operand);
+    void checkNumberOperands(Token& op, TObject& left, TObject& right);
+    // starts and ends for string
+    inline bool startsWith(const std::string& str, const std::string& start) {
+        if (start.size() > str.size()) return false;
+        return str.compare(0, start.size(), start) == 0;
+    }
+
+    inline bool endsWith(const std::string& str, const std::string& end) {
+        if (end.size() > str.size()) return false;
+        return std::equal(end.rbegin(), end.rend(), str.rbegin());
+    }
+
 private: 
     std::string m_result;
-
-    std::string stringify(TObject);
+    // LukError lukErr;
 
     std::string parenthesize(std::string name, std::vector<Expr*> exprs);
     TObject evaluate(PExpr& expr);
+    std::string stringify(TObject obj);
+    bool isTruthy(TObject& obj);
 
 };
 
